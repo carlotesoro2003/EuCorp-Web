ebd0c536922b9a40b9cf5973e7e5db0d3a8d1207
carlotesoro2003 <carlomtesoro@gmail.com>
2024-12-04 23:22:34 +0800

    added svelte config

---
+++ frontend/.vercel/output/static/_app/immutable/entry/start.BktfVmRK.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Dj_Mz2ul.js";export{t as start};
---
+++ frontend/.vercel/output/static/_app/immutable/nodes/7.Bub4mupJ.js
@@ -0,0 +1,1 @@
+import"../chunks/disclose-version.CM6f1PTD.js";import{L as n}from"../chunks/Login.BObmI21P.js";function i(o){n(o,{})}export{i as component};
